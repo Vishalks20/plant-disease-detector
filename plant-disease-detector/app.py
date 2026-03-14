11369b8cf0b8f20b7279871f6b11166b53c17731
Vishalks20 <vishalkumar202005@gmail.com>
2026-03-14 23:11:55 +0530

    Update app.py

--- plant-disease-detector/app.py
+++ plant-disease-detector/app.py
@@ -63,4 +63,4 @@ def create_app(env="development"):
 app = create_app(os.environ.get("FLASK_ENV", "development"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
